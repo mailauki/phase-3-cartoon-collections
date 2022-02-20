c88af09eccb82ad9136dc5576633a9a4c3d8a9f5
Julie Evans <juliemevans17@gmail.com>
2022-02-19 21:09:07 -0600

    commit tree e1254ae0cacc9d859f1fbeaeee2139e564873a61

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves dwarves
+  dwarves.map.with_index do |dwarf, index|
+    puts "#{index+1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+  planeteer_calls.map { |call| call.capitalize << "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls calls
+  calls.map { |call| call.length > 4}.include?(true)
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese ingredients
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  cheese_types.find { |cheese| ingredients.include?(cheese) ? cheese : nil }
 end
